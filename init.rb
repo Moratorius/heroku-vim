5323f55dab4e2d4918dfd34b2ac0446f597827b4
Kyle Chong <kyle@kajabi.com>
2020-10-05 16:06:27 -0700

    Use bundled ca file

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ class Heroku::Command::Vim < Heroku::Command::Run
   def vim_cmd
     <<-CMD
 mkdir vim
-curl https://s3.amazonaws.com/heroku-vim/vim-7.3.tar.gz --location --silent | tar xz -C vim
+CURL_CA_BUNDLE=/app/config/ca-bundle.crt curl https://s3.amazonaws.com/heroku-vim/vim-7.3.tar.gz --location --silent | tar xz -C vim
 export PATH=$PATH:/app/vim/bin
 bash
     CMD
